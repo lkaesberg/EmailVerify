af983098d2506f7c8aeaed3b633f840107b4a09f
Sral248 <33939116+Sral248@users.noreply.github.com>
2022-02-05 18:08:20 +0100

    feat(domains): add multiple domains

--- src/commands/domains.js
+++ src/commands/domains.js
@@ -3,24 +3,30 @@ const database = require("../database/Database.js");
 
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('domains').setDescription('returns registered domains').addStringOption(option => option.setName('domain').setDescription('register given domain')),
+    data: new SlashCommandBuilder().setName('domains').setDescription('returns registered domains').addStringOption(option => option.setName('domain').setDescription('register given domain (add multiple domains separated by \',\')')),
     async execute(interaction) {
-        const domain = interaction.options.getString('domain');
+        const domains = interaction.options.getString('domain');
         await database.getServerSettings(interaction.guildId, async serverSettings => {
-            if (domain === null) {
+            if (domains === null) {
                 await interaction.reply("Allowed domains: " + serverSettings.domains.toString())
             } else {
-                if (domain.startsWith("@") && domain.includes(".")) {
-                    serverSettings.domains.push(domain)
-                    await interaction.reply("Added " + domain)
-                    database.updateServerSettings(interaction.guildId, serverSettings)
-                } else if (!domain.includes("@") && domain.includes(".")) {
-                    serverSettings.domains.push("@" + domain)
-                    await interaction.reply("Added " + "@" + domain)
+                let addedDomains = []
+                domains.split(",").forEach(domain => {
+                    if (domain.startsWith("@") && domain.includes(".")) {
+                        serverSettings.domains.push(domain)
+                        addedDomains.push(domain)
+                    } else if (!domain.includes("@") && domain.includes(".")) {
+                        serverSettings.domains.push("@" + domain)
+                        addedDomains.push("@" + domain)
+                    }
+                })
+                if (addedDomains.length !== 0) {
+                    await interaction.reply("Added " + addedDomains.toString())
                     database.updateServerSettings(interaction.guildId, serverSettings)
                 } else {
                     await interaction.reply("Please enter a valid domain")
                 }
+
             }
         })
 
