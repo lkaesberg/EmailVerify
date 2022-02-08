5ebdc945b84ff89dd6cf4c96c3638908237832dc
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2022-02-08 02:02:18 +0100

    fix(removedomain): try catch

--- src/bot/registerRemoveDomain.js
+++ src/bot/registerRemoveDomain.js
@@ -4,24 +4,28 @@ const {Routes} = require("discord-api-types/v9");
 const {clientId} = require("../../config.json");
 
 module.exports = async function registerRemoveDomain(guildId, removeDomain = require("../commands/removedomain")) {
-    await database.getServerSettings(guildId, async serverSettings => {
-        await rest.get(Routes.applicationGuildCommands(clientId, guildId)).then(async commands => {
-            const commandId = commands.find(command => command.name === "removedomain")?.id
+    try {
+        await database.getServerSettings(guildId, async serverSettings => {
+            await rest.get(Routes.applicationGuildCommands(clientId, guildId)).then(async commands => {
+                const commandId = commands.find(command => command.name === "removedomain")?.id
 
-            if (!commandId) return
+                if (!commandId) return
 
-            let removeDomainCommand = removeDomain.data.toJSON()
+                let removeDomainCommand = removeDomain.data.toJSON()
 
 
-            if (serverSettings.domains.length < 25) {
-                removeDomainCommand["options"][0]["choices"] = serverSettings.domains.map(domain => {
-                    return {"name": domain, "value": domain}
-                })
-            } else {
-                removeDomainCommand["options"][0]["choices"] = undefined
-            }
+                if (serverSettings.domains.length < 25) {
+                    removeDomainCommand["options"][0]["choices"] = serverSettings.domains.map(domain => {
+                        return {"name": domain, "value": domain}
+                    })
+                } else {
+                    removeDomainCommand["options"][0]["choices"] = undefined
+                }
 
-            await rest.patch(Routes.applicationGuildCommand(clientId, guildId, commandId), {body: removeDomainCommand}).catch()
+                await rest.patch(Routes.applicationGuildCommand(clientId, guildId, commandId), {body: removeDomainCommand}).catch()
+            }).catch()
         }).catch()
-    }).catch()
+    }catch (e) {
+        
+    }
 }
