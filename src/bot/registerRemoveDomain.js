800c3cc4b5f5881cfa6728a6d8d251654c1d1815
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2022-02-08 02:11:21 +0100

    feat(removedomain): autocomplete

--- src/bot/registerRemoveDomain.js
+++ src/bot/registerRemoveDomain.js
@@ -4,8 +4,8 @@ const {Routes} = require("discord-api-types/v9");
 const {clientId} = require("../../config.json");
 
 module.exports = async function registerRemoveDomain(guildId, removeDomain = require("../commands/removedomain")) {
-    /*await database.getServerSettings(guildId, async serverSettings => {
-        await rest.get(Routes.applicationGuildCommands(clientId, guildId)).then(async commands => {
+    await database.getServerSettings(guildId, async serverSettings => {
+        rest.get(Routes.applicationGuildCommands(clientId, guildId)).then(async commands => {
             const commandId = commands.find(command => command.name === "removedomain")?.id
 
             if (!commandId) return
@@ -21,7 +21,7 @@ module.exports = async function registerRemoveDomain(guildId, removeDomain = req
                 removeDomainCommand["options"][0]["choices"] = undefined
             }
 
-            await rest.patch(Routes.applicationGuildCommand(clientId, guildId, commandId), {body: removeDomainCommand}).catch()
-        }).catch()
-    }).catch()*/
+            rest.patch(Routes.applicationGuildCommand(clientId, guildId, commandId), {body: removeDomainCommand}).catch()
+        }).catch(e => console.log(e))
+    }).catch(e => console.log(e))
 }
