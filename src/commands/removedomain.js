32c5b3e54bff254d201f899891ce2e159f5bd13a
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2022-02-09 11:55:37 +0100

    feat(removedomain): remove multiple domains

--- src/commands/removedomain.js
+++ src/commands/removedomain.js
@@ -3,15 +3,18 @@ const database = require("../database/Database.js");
 const registerRemoveDomain = require("../bot/registerRemoveDomain")
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('removedomain').setDescription('remove registered domain').addStringOption(option => option.setName('removedomain').setDescription('remove registered domain').setRequired(true)),
+    data: new SlashCommandBuilder().setName('removedomain').setDescription('remove registered domain').addStringOption(option => option.setName('removedomains').setDescription('remove registered domain (remove multiple domains separated by \',\')').setRequired(true)),
     async execute(interaction) {
-        const removeDomain = interaction.options.getString('removedomain', true);
+        const removeDomains = interaction.options.getString('removedomains', true).split(",");
 
         await database.getServerSettings(interaction.guildId, async serverSettings => {
-            serverSettings.domains = serverSettings.domains.filter(function (value) {
-                return value !== removeDomain;
-            });
-            await interaction.reply("Removed " + removeDomain)
+            const deletedDomains = serverSettings.domains.filter((domain) => removeDomains.includes(domain))
+            serverSettings.domains = serverSettings.domains.filter((domain) => !removeDomains.includes(domain));
+            if (deletedDomains.length === 0){
+                await interaction.reply("Removed no domains")
+            }else{
+                await interaction.reply("Removed " + deletedDomains.toString())
+            }
             database.updateServerSettings(interaction.guildId, serverSettings)
             await registerRemoveDomain(interaction.guildId, {data: this.data})
         })
