4251ee0298c85e28cfe42f7c7e866aefe48f2117
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-01-31 16:05:13 +0200

    fix: double quotes, naming, formatter

--- src/commands/blacklist.js
+++ src/commands/blacklist.js
@@ -8,23 +8,25 @@ module.exports = {
     .setDescription('returns registered blacklist')
     .addStringOption(option => option
         .setName('email')
-        .setDescription('register blacklisted emails (add multiple separated by \',\'), (pass "-" to clear)')
+        .setDescription('register blacklisted emails (add multiple separated by ","), (pass "-" to clear)')
         ),
     async execute(interaction) {
         const blacklist = interaction.options.getString('email');
         await database.getServerSettings(interaction.guildId, async serverSettings => {
             if (!blacklist) {
-                await interaction.reply("Blacklisted names: " + serverSettings.blacklist.join(", "));
+                await interaction.reply("Blacklisted emails:\n" + serverSettings.blacklist.join(", "));
             } else if (blacklist === "-") {
                 serverSettings.blacklist=[];
                 database.updateServerSettings(interaction.guildId, serverSettings);
                 await interaction.reply("Blacklist cleared!");
             }
             else {
-                    const blacklistedNames = serverSettings.blacklist.concat(blacklist.split(",").map(name=> name.trim()));
-                    serverSettings.blacklist = blacklistedNames;
+                    const newBlacklists = blacklist.split(",").map(name=> name.trim())
+                    const blacklistedNames = (format = false) => serverSettings.blacklist
+                        .concat(format ? newBlacklists.map(v=> `**${v}**`) : v);
+                    serverSettings.blacklist = blacklistedNames();
                     database.updateServerSettings(interaction.guildId, serverSettings);
-                    await interaction.reply("Added " + blacklistedNames.join(", "));
+                    await interaction.reply("Added:\n" + blacklistedNames(true).join(", "));
             }
         })
 
